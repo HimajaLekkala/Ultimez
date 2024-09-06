f9cd287db42b41e2e9508f221332baa20a2bacd1
himaja <himajalekkala@himajas-MacBook-Pro.local>
2024-09-06 20:17:33 +0530

    Initial commit:

---
+++ UltimezSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltimezSample
+//
+//  Created by himaja on 06/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltimezSample/UltimezSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimezSampleApp.swift
+//  UltimezSample
+//
+//  Created by himaja on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimezSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
